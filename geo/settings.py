412aec158238124ddded1a2ed1fa4f786ac4509c
andddias <and.ddias@gmail.com>
2020-02-28 14:12:11 -0300

    Configurações Iniciais

--- geo/settings.py
+++ geo/settings.py
@@ -25,12 +25,13 @@ SECRET_KEY = '7s#py=56$19=owvuu+qizw%450^sp8+@h^sov!h95ym_xk$p^c'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    'core',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'geo.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -103,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
@@ -118,3 +119,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+YELP_API_KEY = '51PlimKhpSyjnUH-w9zghTy18Vn2eR5GYZjTBZoHxKnjvIjsNw5sg0YqhLeUXh0gH14DU5pagAl0yyx7aO6eIsdS91951CEmh9f8QxJWHxU5Njiu2rmaXp9g2UhZXnYx'
+
+GEOIP_PATH = os.path.join(BASE_DIR, 'geoip')
+
+LOGOUT_REDIRECT_URL = 'index'
